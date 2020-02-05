7941b248884acd6738e6826faecc738c4f1ed9f2
Maria Traskowsky <mariatraskowsky@users.campus>
2020-02-05 14:51:52 -0600

    more changes

--- DoesItBlend/Strawberry.cs
+++ DoesItBlend/Strawberry.cs
@@ -7,13 +7,13 @@ namespace DoesItBlend
     /// <summary>
     /// A class representing a Strawberry
     /// </summary>
-    public class Strawberry : Fruit
+    public class Strawberry : Fruit, IBlendable // fruit class already inherits iblendable so you don't have to add iblendable to this class, but you can 
     {
         /// <summary>
         /// Blends the strawberry
         /// </summary>
         /// <returns>The result of blending a strawberry</returns>
-        public override string Blend()
+        public string Blend()
         {
             return "Gooey Red Sweetness";
         }
